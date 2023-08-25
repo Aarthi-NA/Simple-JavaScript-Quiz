796757dee88ba2179ec819232bbb597ea364fca2
Aarthi <717821l101@kce.ac.in>
2023-08-25 23:09:32 +0530

    initialcommit

---
+++ quest.js
@@ -0,0 +1,91 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which statement cannot be used to declare a variable in JavaScript?",
+    answer: "Int",
+    options: [
+      "let",
+      "Var",
+      "Int",
+      "Const"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is block scoped?",
+    answer: "Both b and c",
+    options: [
+      "Var",
+      "let",
+      "Const",
+      "Both b and c"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following is correct about JavaScript?",
+    answer: "JavaScript is an Object-Based language",
+    options: [
+      'JavaScript is an Object-Based language',
+      'JavaScript is Assembly-language',
+      'JavaScript is an Object-Oriented language',
+      'JavaScript is a High-level language'
+    ]
+  },
+    {
+    numb: 4,
+    question: "The 'function' and 'var' are known as:",
+    answer: "Declaration statements",
+    options: [
+      'Keywords',
+      'Data types',
+      'Declaration statements',
+      'Prototypes'
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following variables takes precedence over the others if the names are the same?",
+    answer: "The local element",
+    options: [
+      'Global variable',
+      'The local element',
+      'The two of the above',
+      'None of the above'
+    ]
+  },
+  
+    {
+    numb: 6,
+    question: "Which of the following number object function returns the value of the number?",
+    answer: "valueOf()",
+    options: [
+      'toString()',
+      'valueOf()',
+      'toLocaleString()',
+      'toPrecision()'
+    ]
+  },
+  {
+    numb: 7,
+    question: "If you type the following code in the console window, what result will you get? 3 > 2 > 1 === false",
+    answer: "True",
+    options: [
+      "True",
+      "False"
+    ]
+  },
+  {
+    numb: 8,
+    question: "How do you find the minimum of x and y using JavaScript?",
+    answer: "Math.min(x,y)",
+    options: [
+      "min(x,y)",
+      "Math.min(x,y)",
+      "Math.min(xy)",
+      "min(xy)"
+    ]
+  },
+  ];
+  
